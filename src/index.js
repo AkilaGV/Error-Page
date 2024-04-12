4194420e0a5ad8ee8484789474172ffcc2ac6a23
AkilaGV <akilagv1997@gmail.com>
2024-04-12 22:45:16 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import react from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+function Error(){
+  return(<div >
+    <div className='text-white container'>
+      <h1 className="font-medium text-2xl "> 404 Error Page</h1>
+      
+      <p>| Sorry The Page are not found |
+      <i class="fa-light fa-star"></i></p>
+      <button className="text-white ">GO Home </button>
+
+      
+      
+    </div>
+    </div>
+    
+  )
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Error></Error>
+)
